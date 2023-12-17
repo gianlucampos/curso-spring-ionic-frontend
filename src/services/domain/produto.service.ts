54419c4a4bdd6101630a7ab44b8527c2cfe70d43
gianlucampos <gianlucampos97@gmail.com>
2023-12-17 15:19:40 -0300

    Carregando dados do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -10,12 +10,21 @@ export class ProdutoService {
   constructor(public http: HttpClient) {
   }
 
-  findByCategoria(categoria_id: string): Observable<ProdutoDTO[]> {
-    return this.http.get<ProdutoDTO[]>(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+  findById(produto_id: string) {
+    return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+  }
+
+  findByCategoria(categoria_id: string) {
+    return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
   }
 
   getSmallImageFromBucket(id: string): Observable<any> {
     const url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+    return this.http.get(url, {responseType: 'blob'});
+  }
+
+  getImageFromBucket(id: string): Observable<any> {
+    const url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`;
     return this.http.get(url, {responseType: 'blob'});
   }
 
