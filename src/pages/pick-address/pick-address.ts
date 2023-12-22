9a0f7bcb929c4e69c8e2d3e350e158f4f2108044
Gianluca Campos <gianlucampos97@gmail.com>
2023-12-22 20:22:33 -0300

    Tela de escolha de pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -52,5 +52,6 @@ export class PickAddressPage {
 
   nextPage(item: EnderecoDTO) {
     this.pedido.enderecoDeEntrega = {id: item.id};
+    this.navCtrl.push('PaymentPage', {pedido: this.pedido});
   }
 }
